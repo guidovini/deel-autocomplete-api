4ad0253d0f7c10c353d1d3eb73a8df41a36328b7
Guido Santillan <guidosantillan01@gmail.com>
2024-04-24 09:55:25 +0200

    edit cors policies

--- src/main.ts
+++ src/main.ts
@@ -5,7 +5,7 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors({
-    origin: ['http://localhost:5173', 'http://localhost:4173'], // Specific allowed origin
+    origin: ['https://deel-autocomplete.onrender.com'], // Specific allowed origin
     methods: 'GET', // Allowed methods
     credentials: true, // Allow sending of cookies, if needed for authentication
   });
