20439f62dcdf887abeed90f7bef0fc4ae41810b2
Guido Santillan <guidosantillan01@gmail.com>
2024-04-24 10:57:41 +0200

    add env variables

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,18 @@
 import { NestFactory } from '@nestjs/core';
+import { ConfigModule } from '@nestjs/config';
 
 import { AppModule } from './app.module';
+
+ConfigModule.forRoot();
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors({
-    origin: ['https://deel-autocomplete.onrender.com'], // Specific allowed origin
+    origin: [
+      process.env.DEV_CLIENT,
+      process.env.DEV_PROD_CLIENT,
+      process.env.PROD_CLIENT,
+    ], // Specific allowed origin
     methods: 'GET', // Allowed methods
     credentials: true, // Allow sending of cookies, if needed for authentication
   });
